b5b40e8d252b04ffb80fd4efda6e69fbbbf0ddfd
Gode King <GengGode@outlook.com>
2022-04-03 01:25:48 +0800

    实现了ui整理，适配小窗口

--- 空荧酒馆-悬浮窗/YuanShenSite_悬浮窗.xaml.cs
+++ 空荧酒馆-悬浮窗/YuanShenSite_悬浮窗.xaml.cs
@@ -25,6 +25,79 @@ namespace 空荧酒馆_悬浮窗
         public YuanShenSite_悬浮窗()
         {
             this.InitializeComponent();
+            this.Loaded += MainPage_Loaded;
+        }
+
+        private async void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            await webView.EnsureCoreWebView2Async();
+            webView.CoreWebView2.DOMContentLoaded += CoreWebView2_DOMContentLoaded;
+        }
+
+        private void CoreWebView2_DOMContentLoaded(Microsoft.Web.WebView2.Core.CoreWebView2 sender, Microsoft.Web.WebView2.Core.CoreWebView2DOMContentLoadedEventArgs args)
+        {
+            string js = @"  
+                            let cursor = document.createElement('div')
+                            cursor.id = 'inject-user-cursor'
+                            window.injectCursor = cursor 
+                            document.body.appendChild(cursor)
+
+                            let style = document.createElement('style')
+                            style.innerHTML = `
+                                .danjixiazai, .enindex-btn, .jpindex-btn, .switch_content, .fankui, .genshin_pub, .genshin_art, .feixiaoqiu, .wiki{
+                                    display: none!important;
+                                } 
+                                .control-containor{
+                                    transform:scale(0.4);
+                                }
+                                #inject-user-cursor{
+                                    position: fixed;
+                                    visibility: hidden;
+                                    left: 50vw;
+                                    top: 50vh;
+                                    width: 36px;
+                                    height: 36px;
+                                    transform: translate(-50%, -50%);
+                                    transition: all .5s;
+                                    transform-origin: center;
+                                    z-index: 999999999999999;
+                                    background-image: url(https://assets.yuanshen.site/icons/483.png);
+                                    background-repeat: no-repeat;                                
+                                    background-position: center;        
+                                }  
+                            `
+                            document.head.appendChild(style)
+                        ";
+            webView.ExecuteScriptAsync(js);
+        }
+
+        private async void WebView_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void flyto(float x, float y)
+        {
+
+        }
+
+        private void A_Click(object sender, RoutedEventArgs e)
+        {
+            string js = @"map.flyTo([4000,5000]);";
+            webView.ExecuteScriptAsync(js);
+        }
+        private void B_Click(object sender, RoutedEventArgs e)
+        {
+            string js = @"window.injectCursor.style.left = '200px';
+                           window.injectCursor.style.top = '200px';
+                        window.injectCursor.style.visibility = 'visible'
+                        ";
+            webView.ExecuteScriptAsync(js);
+        }
+        private void C_Click(object sender, RoutedEventArgs e)
+        {
+            string js = @"window.injectCursor.style.transform = 'translate(-50%, -50%) rotate(-30deg)';";
+            webView.ExecuteScriptAsync(js);
         }
     }
 }
